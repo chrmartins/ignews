a9824945f2dbe5179d9750d6e1838ab87fd23a8a
Christian Martins <cmts2010@gmail.com>
2021-05-14 22:50:43 -0300

    feat: redirection subscribe

---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+﻿import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: '/api'
+})
---
+++ src/services/fauna.ts
@@ -0,0 +1,5 @@
+﻿import { Client } from 'faunadb'
+
+export const fauna = new Client({
+  secret: process.env.FAUNADB_KEY
+})
