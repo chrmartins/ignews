948b4210eb0590d13c1fcb24f3748137319f9ffd
Christian Martins <cmts2010@gmail.com>
2021-06-15 16:34:21 -0300

    feat: webhooks

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,8 @@
+﻿import { NextApiRequest } from 'next'
+import { NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  console.log('evento recebido')
+
+  res.status(200).json({ ok: true });
+}
